541e4e80cc3977bd4bd2bcfc40f0953290b98166
Madhumathy <63185495+madhumathykumaran@users.noreply.github.com>
2023-09-11 12:03:43 -0700

    update rectangle.py

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,7 +5,29 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
+from abc import ABC, abstractmethod
+ 
+class Shape(ABC):
+ 
+    @abstractmethod
+    def set_values(self):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, x, y):
+        self.width = x
+        self.height = y
+
+    def get_width(self):
+        return self.width
+    
+    def get_height(self):
+        return self.height
+    
     def set_values(self, x, y):
         self.width = x
         self.height = y
@@ -16,10 +38,12 @@ class Rectangle:
 
 if __name__ == "__main__":
     # Create a rectangle object
-    rect = Rectangle()
+    rect = Rectangle(2, 3)
 
     # Call a member function
-    rect.set_values(3, 4)
+    rect.set_values(4, 6)
 
     # Print out the area function
+    print(rect.get_height())
+    print( issubclass(Rectangle, Shape))
     print("area:", rect.area())
