b2898c8098edd5adc5a3268df19dd3c3af612cd3
renyuxuan1998 <ren.yuxu@northeastern.edu>
2023-09-13 18:50:30 -0700

    refactor push

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,12 +5,45 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
-    # git diff will show the change
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    
+    @abstractmethod
+    def set_values(self, x, y):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+    
+class Rectangle(Shape):
+    # git diff will show the change1
     # git push -u origin refactor      #first push
     def __init__(self, width, height):
-        self._width = width
-        self._height = height
+        # Make Private Variable
+        self.__width = width
+        self.__height = height
+    
+    # Getter for width
+    @property    
+    def width(self):
+        return self.__width
+    
+    # Setter for width
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        
+    # Getter for height
+    @property
+    def height(self):
+        return self.__height
+    
+    # Setter for height
+    @height.setter
+    def height(self, value):
+        self.__height = value
         
     def set_values(self, x, y):
         self.width = x
@@ -22,8 +55,10 @@ class Rectangle:
 
 if __name__ == "__main__":
     # Create a rectangle object
-    rect = Rectangle()
+    rect = Rectangle(4, 5)
 
+    print("area:", rect.area())
+    
     # Call a member function
     rect.set_values(3, 4)
 
