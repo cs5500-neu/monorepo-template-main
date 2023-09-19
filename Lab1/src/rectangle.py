c9d5a92e69ce392886dd96ff24abbeb241787b44
Julianlee861112 <lee.yu-y@northeastern.edu>
2023-09-18 17:25:40 -0700

    Added hw1

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,13 +5,46 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
-    def set_values(self, x, y):
-        self.width = x
-        self.height = y
+
+# Abstract class Shape, which has two methods set_values and area
+
+
+class Shape:
+    @abstractmethod
+    def set_values(self, width, height):
+        pass
+
+    def area(self):
+        pass
+
+# Class Rectangle extends class Shape, which has two private variables width, height
+
+
+class Rectangle(Shape):
+    # Constructor for rectangle
+    def set_values(self, width, height):
+        self.width = width
+        self.height = height
+    # Function to return the area of rectangle
 
     def area(self):
         return self.width * self.height
+    # getter for Width
+
+    def getWidth(self):
+        return self.width
+    # getter for height
+
+    def getHeight(self):
+        return self.height
+    # setter for width
+
+    def setWidth(self, width):
+        self.width = width
+    # setter for height
+
+    def setHeight(self, height):
+        self.height = height
 
 
 if __name__ == "__main__":
